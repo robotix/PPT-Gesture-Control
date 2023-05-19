30cd17af1d28821a77893710a838032d2cd03699
Prasanna <paithankarprasanna@gmail.com>
2023-05-20 01:06:16 +0530

    Changes to be committed: 	new file:   function_module.py 	new file:   main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from function_module import *
+
+def main()->None:
+    '''
+    The main function
+    '''
+    # Google meet stream input
+    # analyze frames and execute actions
+    return None
+
+if __name__ == "__main__":
+    main()
