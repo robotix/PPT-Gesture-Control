62edd2491ddd8a5eee41fcd26045ec356d4f8694
Prasanna <paithankarprasanna@gmail.com>
2023-08-06 04:53:24 +0530

    Changes to be committed: 	modified:   ppt-control/Script/function_module.py

--- ppt-control/Script/function_module.py
+++ ppt-control/Script/function_module.py
@@ -7,10 +7,12 @@ from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import pyautogui
 
+
 class HandGestureRecognizer():
     def __init__(self, mode=False, maxHands=2, modelC=1, detectionCon=0.5, trackCon=0.5):
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(mode, maxHands, modelC, detectionCon, trackCon)
+        self.hands = self.mpHands.Hands(
+            mode, maxHands, modelC, detectionCon, trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
     def initialize(self, img):
@@ -23,7 +25,8 @@ class HandGestureRecognizer():
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for handLms in self.results.multi_hand_landmarks:
-                self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+                self.mpDraw.draw_landmarks(
+                    img, handLms, self.mpHands.HAND_CONNECTIONS)
             for id, lm in enumerate(myHand.landmark):
                 h, w, z = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
@@ -31,94 +34,128 @@ class HandGestureRecognizer():
                 cv2.circle(img, (cx, cy), 5, (255, 255, 0), cv2.FILLED)
         return img
 
+
 class PoseGestureRecognizer():
     def __init__(self, model_complexity=1, smooth_landmarks=True, enable_segmentation=False, smooth_segmentation=True, min_detection_confidence=0.5, min_tracking_confidence=0.5):
         self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose(model_complexity, smooth_landmarks, enable_segmentation, smooth_segmentation, min_detection_confidence, min_tracking_confidence)
+        self.pose = self.mpPose.Pose(model_complexity, smooth_landmarks, enable_segmentation,
+                                     smooth_segmentation, min_detection_confidence, min_tracking_confidence)
         self.mpDraw = mp.solutions.drawing_utils
 
         self.devices = AudioUtilities.GetSpeakers()
-        self.interface = self.devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        self.interface = self.devices.Activate(
+            IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
         self.volume = cast(self.interface, POINTER(IAudioEndpointVolume))
-        self.volumeThreshold=0.2
-        self.scale=30
+        self.volumeThreshold = 0.2
+        self.scale = 30
 
     def initialize(self, img):
         self.crossThreshold = 1
         self.verticalThreshold = 1
         try:
-            self.lmList = self.pose.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
-            self.crossThreshold = np.abs((self.lmList[12].x - self.lmList[11].x)/(self.lmList[12].y - self.lmList[24].y))
-            self.verticalThreshold = np.abs((self.lmList[8].y - self.lmList[12].y)/(self.lmList[12].x - self.lmList[11].x))
+            self.lmList = self.pose.process(cv2.cvtColor(
+                img, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
+            self.crossThreshold = np.abs(
+                (self.lmList[12].x - self.lmList[11].x)/(self.lmList[12].y - self.lmList[24].y))
+            self.verticalThreshold = np.abs(
+                (self.lmList[8].y - self.lmList[12].y)/(self.lmList[12].x - self.lmList[11].x))
             print(self.crossThreshold)
             print(self.verticalThreshold)
         except:
             pass
 
-    def findPosition(self, img)->None:
+    def findPosition(self, img) -> None:
         self.lmList = []
         imgRBG = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.pose.process(imgRBG)
         if self.results.pose_landmarks:
-            self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+            self.mpDraw.draw_landmarks(
+                img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 h, w, z = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 self.lmList.append([id, cx, cy])
                 cv2.circle(img, (cx, cy), 5, (255, 255, 0), cv2.FILLED)
-        
+
         return img
-    
+
     def volumeControl(self, imgPrev, imgCurr):
         try:
-            self.lmList = self.pose.process(cv2.cvtColor(imgPrev, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
+            self.lmList = self.pose.process(cv2.cvtColor(
+                imgPrev, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
             if self.lmList:
-                if (self.lmList[15].visibility>0.5 and self.lmList[16].visibility>0.5):
+                if (self.lmList[15].visibility > 0.5 and self.lmList[16].visibility > 0.5):
                     if (np.abs((np.abs(float((self.lmList[15].x-self.lmList[16].x))))) < self.volumeThreshold):
-                        self.curr=self.volume.GetMasterVolumeLevel()
-                        self.prev_diff=(np.abs(float((self.lmList[15].y-self.lmList[16].y))))
-                        self.lmList = self.pose.process(cv2.cvtColor(imgCurr, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
-                        self.curr_diff=(np.abs(float((self.lmList[15].y-self.lmList[16].y))))
-                        self.curr+=(float(self.curr_diff)-float(self.prev_diff))*self.scale
-                        if (self.curr>-65.0 and self.curr<0.0):
+                        self.curr = self.volume.GetMasterVolumeLevel()
+                        self.prev_diff = (
+                            np.abs(float((self.lmList[15].y-self.lmList[16].y))))
+                        self.lmList = self.pose.process(cv2.cvtColor(
+                            imgCurr, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
+                        self.curr_diff = (
+                            np.abs(float((self.lmList[15].y-self.lmList[16].y))))
+                        self.curr += (float(self.curr_diff) -
+                                      float(self.prev_diff))*self.scale
+                        if (self.curr > -65.0 and self.curr < 0.0):
                             self.volume.SetMasterVolumeLevel(self.curr, None)
                         else:
                             pass
         except:
             pass
 
-    def slideControl(self, imgPrev, imgCurr):
+    def get_ratio_shoulder_height(self, img):
         try:
-            self.lmListPrev = self.pose.process(cv2.cvtColor(imgPrev, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
-            self.lmListCurr = self.pose.process(cv2.cvtColor(imgCurr, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
-
-            self.crossThreshold = 0.17205
-            self.verticalThreshold = 0.7
-            if self.lmListPrev and self.lmListCurr:
-                if (self.lmListPrev[15]).x > (self.lmListPrev[9]).x and (self.lmListCurr[15]).x < (self.lmListCurr[9]).x:
-                    # print((self.lmListCurr[15]).x - (self.lmListCurr[12]).x)
-                    if (np.abs(float((self.lmListCurr[15]).x - (self.lmListCurr[12]).x))) > self.crossThreshold/2:
-                        # if (self.lmListPrev[15]).y < (self.lmListPrev[11]).y and ((self.lmListPrev[15]).y > (self.lmListPrev[7]).y - self.verticalThreshold/2):
-                        pyautogui.press('right')
-                        time.sleep(0.5)
-
-                elif (self.lmListPrev[16]).x < (self.lmListPrev[10]).x and (self.lmListCurr[16]).x > (self.lmListCurr[10]).x:
-                    if (np.abs(float((self.lmListCurr[16]).x - (self.lmListCurr[11]).x))) > self.crossThreshold/2:
-                        # if (self.lmListPrev[16]).y > (self.lmListPrev[12]).y and ((self.lmListPrev[16]).y < (self.lmListPrev[8]).y + self.verticalThreshold/2):
-                        pyautogui.press('left')
-                        time.sleep(0.5)
-                
+            newlmList = self.pose.process(cv2.cvtColor(
+                img, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
+            shoulder = np.abs(newlmList[12].x - newlmList[11].x)
+            height = np.abs(newlmList[0].y - newlmList[24].y)
+            ratio = shoulder/height
+            return ratio
         except:
             pass
         return None
-    
 
-def main()->None:
+    def get_shoulder_to_ear(self, img):
+        try:
+            newlmList = self.pose.process(cv2.cvtColor(
+                img, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
+            dist = np.abs(newlmList[12].y - newlmList[8].x)
+            return dist
+        except:
+            pass
+        return None
+
+    def slideControl(self, imgPrev, imgCurr):
+        try:
+            self.lmListPrev = self.pose.process(cv2.cvtColor(
+                imgPrev, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
+            self.lmListCurr = self.pose.process(cv2.cvtColor(
+                imgCurr, cv2.COLOR_BGR2RGB)).pose_landmarks.landmark
+
+            if self.lmListPrev and self.lmListCurr:
+                if (self.lmListPrev[15]).x > (self.lmListPrev[9]).x and (self.lmListCurr[15]).x < (self.lmListCurr[9]).x:
+                    if self.get_ratio_shoulder_height(imgCurr) > self.crossThreshold/1.41:
+                        if (self.lmListPrev[15]).y < (self.lmListPrev[11]).y + self.get_shoulder_to_ear(imgCurr) and ((self.lmListCurr[15]).y > (self.lmListCurr[7]).y - self.get_shoulder_to_ear(imgCurr)/1.41):
+                            pyautogui.press('right')
+                            time.sleep(0.5)
+
+                elif (self.lmListPrev[16]).x < (self.lmListPrev[10]).x and (self.lmListCurr[16]).x > (self.lmListCurr[10]).x:
+                    if self.get_ratio_shoulder_height(imgCurr) > self.crossThreshold/1.41:
+                        if (self.lmListPrev[16]).y < (self.lmListPrev[12]).y + self.get_shoulder_to_ear(imgCurr) and ((self.lmListCurr[16]).y > (self.lmListCurr[8]).y - self.get_shoulder_to_ear(imgCurr)/1.41):
+                            pyautogui.press('left')
+                            time.sleep(0.5)
+
+        except:
+            pass
+        return None
+
+
+def main() -> None:
     '''
     The main function
     '''
     print("Library module for PPT control")
     return None
-    
+
+
 if __name__ == "__main__":
     main()
